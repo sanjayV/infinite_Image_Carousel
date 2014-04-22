8c59d8b9f85a4da8e0033b3b26ac848f1f9121f7
sanjay <sanjay@localhost.localdomain>
2014-04-22 11:13:19 +0530

    Added Infinite carousel Files

---
+++ script.js
@@ -0,0 +1,81 @@
+carousel =  {
+	imgWidth: '1353',
+	currentImg: 0,
+	newImg: 0,
+	totalImg: 0,
+	mainClass: '.inner',
+	innerClass: '.imageDiv',
+	carouselWidth: 0,
+	animateSpeed: 500,
+
+	init: function() {
+		this.totalImg = $(this.mainClass).find(this.innerClass).length;
+		this.getCarouselPosition();
+		this.bindEvents();
+	},
+
+	bindEvents: function() {
+		var _this = this;
+		$('.prev').unbind('click').bind('click', function() {
+			_this.newImg = _this.currentImg - 1;;
+
+			if (_this.newImg < 0) {
+				_this.newImg = _this.totalImg - 1;
+			}
+
+			_this.slideCarousel('prev');
+		});
+
+		$('.next').unbind('click').bind('click', function() {
+			_this.newImg = _this.currentImg + 1;
+
+			if (_this.newImg > _this.totalImg - 1) {
+				_this.newImg = 0;
+			}
+
+			console.log(_this.newImg, _this.totalImg);
+
+			_this.slideCarousel('next');
+		});
+	},
+
+	getCarouselPosition: function() {
+		var _this = this;
+		_this.carouselWidth = $(_this.mainClass).find(_this.innerClass).width();
+		console.log(_this.carouselWidth)
+	},
+
+	slideCarousel: function(direction) {
+		var _this = this;
+
+		if (direction == 'next') {
+			$(_this.mainClass).find(_this.innerClass).eq(_this.newImg).css({'left': _this.carouselWidth+'px'}).addClass('active');
+
+			$(_this.mainClass).find(_this.innerClass).eq(_this.newImg).animate({
+				left: 0,
+			}, _this.animateSpeed);
+
+			$(_this.mainClass).find(_this.innerClass).eq(_this.currentImg).animate({
+				left: -1*(_this.carouselWidth),
+			}, _this.animateSpeed, function() {
+				$(this).attr('style', "");
+				$(_this.mainClass).find(_this.innerClass).eq(_this.currentImg).removeClass('active');
+				_this.currentImg = _this.newImg;
+			});
+		} else {
+			$(_this.mainClass).find(_this.innerClass).eq(_this.newImg).css({'left': -1*_this.carouselWidth+'px'}).addClass('active');
+
+			$(_this.mainClass).find(_this.innerClass).eq(_this.newImg).animate({
+				left: 0,
+			}, _this.animateSpeed);
+
+			$(_this.mainClass).find(_this.innerClass).eq(_this.currentImg).animate({
+				left: (_this.carouselWidth),
+			}, _this.animateSpeed, function() {
+				$(_this.mainClass).find(_this.innerClass).eq(_this.currentImg).removeClass('active');
+				$(this).attr('style', "");
+				_this.currentImg = _this.newImg;
+			});
+		}
+	}
+};

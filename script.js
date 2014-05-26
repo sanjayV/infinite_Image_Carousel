2cab351d2425addf85bee07baf7e529a42801044
sanjay <sanjay@localhost.localdomain>
2014-05-26 18:49:47 +0530

    code refactor

--- script.js
+++ script.js
@@ -27,11 +27,11 @@ carousel =  {
 
 		$('.arrowDiv.left').unbind('click').bind('click', function(e) {
 			e.preventDefault();
-			/*_this.newImg = _this.currentImg - 1;;
+			_this.newImg = _this.currentImg - 1;;
 
 			if (_this.newImg < 0) {
 				_this.newImg = _this.totalImg - 1;
-			}*/
+			}
 
 			if (_this.isComplete)
 				_this.slideCarousel('prev');
@@ -39,11 +39,11 @@ carousel =  {
 
 		$('.arrowDiv.right').unbind('click').bind('click', function(e) {
 			e.preventDefault();
-			/*_this.newImg = _this.currentImg + 1;
+			_this.newImg = _this.currentImg + 1;
 
 			if (_this.newImg > _this.totalImg - 1) {
 				_this.newImg = 0;
-			}*/
+			}
 
 			if (_this.isComplete)
 				_this.slideCarousel('next');
@@ -66,58 +66,42 @@ carousel =  {
 		var _this = this;
 		_this.isComplete = false;
 
-		if (direction == 'next') {
-			_this.newImg = _this.currentImg + 1;
+		var newImgMove = _this.carouselWidth+'px'
+			, oldImgMove = -1*(_this.carouselWidth) + 'px';
 
-			if (_this.newImg > _this.totalImg - 1) {
-				_this.newImg = 0;
-			}
 
-			$(_this.mainClass).find(_this.innerClass).eq(_this.newImg).css({'left': _this.carouselWidth+'px'}).addClass('active');
-
-			$(_this.mainClass).find(_this.innerClass).eq(_this.newImg).animate({
-				left: 0,
-			}, _this.animateSpeed);
-
-			$(_this.mainClass).find(_this.innerClass).eq(_this.currentImg).animate({
-				left: -1*(_this.carouselWidth),
-			}, _this.animateSpeed, function() {
-				$(this).attr('style', "");
-				$(_this.mainClass).find(_this.innerClass).eq(_this.currentImg).removeClass('active');
-				_this.currentImg = _this.newImg;
-
-				_this.isComplete = true;
-			});
-		} else {
-			_this.newImg = _this.currentImg - 1;;
-
-			if (_this.newImg < 0) {
-				_this.newImg = _this.totalImg - 1;
-			}
-
-			$(_this.mainClass).find(_this.innerClass).eq(_this.newImg).css({'left': -1*_this.carouselWidth+'px'}).addClass('active');
-
-			$(_this.mainClass).find(_this.innerClass).eq(_this.newImg).animate({
-				left: 0,
-			}, _this.animateSpeed);
-
-			$(_this.mainClass).find(_this.innerClass).eq(_this.currentImg).animate({
-				left: (_this.carouselWidth),
-			}, _this.animateSpeed, function() {
-				$(_this.mainClass).find(_this.innerClass).eq(_this.currentImg).removeClass('active');
-				$(this).attr('style', "");
-				_this.currentImg = _this.newImg;
-
-				_this.isComplete = true;
-			});
+		if (direction == 'prev') {
+			newImgMove = -1*(_this.carouselWidth) + 'px';
+			oldImgMove = _this.carouselWidth + 'px';
 		}
+
+		$(_this.mainClass).find(_this.innerClass).eq(_this.newImg).css({'left': newImgMove}).addClass('active');
+
+		$(_this.mainClass).find(_this.innerClass).eq(_this.newImg).animate({
+			left: 0,
+		}, _this.animateSpeed);
+
+		$(_this.mainClass).find(_this.innerClass).eq(_this.currentImg).animate({
+			left: oldImgMove,
+		}, _this.animateSpeed, function() {
+			$(this).attr('style', "");
+			$(_this.mainClass).find(_this.innerClass).eq(_this.currentImg).removeClass('active');
+			_this.currentImg = _this.newImg;
+
+			_this.isComplete = true;
+		});
 	},
 
 	autoSlide: function() {
 		var _this = this;
 
 		_this.currentInterval = setInterval(function() {
-			console.log(_this.currentImg)
+			_this.newImg = _this.currentImg + 1;
+
+			if (_this.newImg > _this.totalImg - 1) {
+				_this.newImg = 0;
+			}
+
 			_this.slideCarousel('next');
 		}, _this.autoSpeed);
 	}
